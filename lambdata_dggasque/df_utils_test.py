074630008b89edfd4ca756b549c975eddfcad2de
dggasque <dggasque@users.noreply.guthub.com>
2019-12-05 13:24:30 -0800

    Create df_utils_test.py

---
+++ lambdata_dggasque/df_utils_test.py
@@ -0,0 +1,12 @@
+import unittest 
+from df_utils import TEST_DF, Processor
+
+class ProcessorTests(unittest.TestCase):
+    """Test Functions in Processor class!"""
+    def test_missingvalues(self):
+        proc = Processor()
+        df = TEST_DF
+        self.assertEqual(proc.missing_values(df), None)
+
+if __name__ == '__main__':
+    unittest.main()
